331298e22202d2af932dd976f7e9a2be59a3b47b
gabking7 <deleon0072@hotmail.com>
2022-01-20 10:04:43 -0400

    Idk bro...

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,43 +1,34 @@
 function computerPlay() {
-    let rock = String.raw `   
- _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)`;
-
-
-let paper = String.raw `   
- _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________);
-`
-
-
-let scissors = String.raw `    
-_______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)`;
-
-    let rpsArray = [
-        rock,
-        paper,
-        scissors
+    const rpsArray = [
+        "rock",
+        "paper",
+        "scissors"
     ];
 
-    let randomRPS = (Math.floor(Math.random() * rpsArray.length));
+    const randomRPS = (Math.floor(Math.random() * rpsArray.length)); {
+        console.log(`CPU played: ${rpsArray[randomRPS]}`);
         return rpsArray[randomRPS];
-        
-
+    }
 }
 
-function oneRound(playerSelection, computerSelection) {
-    
+
+
+function roundOne(playerSelection, computerSelection) {
+    let userChoice = prompt("Choose rock, paper or scissors!").toLowerCase();
+    console.log(`User played: ${userChoice}`);
+
+    if (userChoice === computerSelection) {
+        return "It's a tie.";
+    } else if (userChoice == "rock" && computerSelection == "scissors") {
+        return "You win!";
+    } else if (userChoice == "paper" && computerSelection == "rock") {
+        return "You win!";
+    } else if (userChoice == "scissors" && computerSelection == "paper") {
+        return "You win!";
+    } else {
+        return "You lose. :(";
+    }
 }
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(roundOne(playerSelection, computerSelection));
